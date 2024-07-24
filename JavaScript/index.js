39f1fadd02f12a68a36e2d159ff2b6940ff8172b
Chakayro <timomass_vs_tilt@hotmail.com>
2024-07-24 15:28:43 -0600

    No habia hecho commit pero si el trabajo, pueden preguntar a los compañeros ya que ayude a varios en discord con su trabajo, no realice cambios desde la fecha de entrega

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -197,17 +197,117 @@ const data = {
 
 let tarjeta = document.getElementsByClassName("card");
 let copia = tarjeta[0];
+let Checkbox = document.getElementsByClassName("checkers");
+let copiaCheckbox = Checkbox[0];
+contador = 0;
+let noHay = document.getElementById("noHay");
+
 
 while (tarjeta.length > 0) {
   tarjeta[0].parentNode.removeChild(tarjeta[0]);
 }
 
-for (let i = 0; i < data.events.length; i++) {
-  let copia2 = copia.cloneNode(true);
-  let nuevasTarjetas = document.getElementById("tarjetas").appendChild(copia2);
-    nuevasTarjetas.querySelector("h5").innerHTML = data.events[i].name;
-        nuevasTarjetas.querySelector("img").src = data.events[i].image;
-        nuevasTarjetas.getElementsByClassName("card-text")[0].innerHTML = data.events[i].description;  
-        nuevasTarjetas.getElementsByClassName("card-text")[1].innerText = "Price $" + data.events[i].price;
+data.events.forEach((event) => {
+  let nuevasTarjetas = document.getElementById("tarjetas").appendChild(copia.cloneNode(true));
+  nuevasTarjetas.querySelector("h5").innerHTML = event.name;
+  nuevasTarjetas.querySelector("img").src = event.image;
+  nuevasTarjetas.getElementsByClassName("card-text")[0].innerHTML = event.description;
+  nuevasTarjetas.getElementsByClassName("card-text")[1].innerText = "Price $" + event.price;
+  let nuevaClase = event.category.replace(/ /g, "");
+  nuevasTarjetas.setAttribute("class", "card " + nuevaClase + " CY");
+  nuevaClase = nuevasTarjetas.className + " TY";
+  nuevasTarjetas.setAttribute("class", nuevaClase);
+});
+
+while (Checkbox.length > 0) {
+  Checkbox[0].parentNode.removeChild(Checkbox[0]);
 }
 
+data.events.forEach((event, index) => {
+  if (index === 0 || event.category !== data.events[index - 1].category) {
+    let nuevosCheckers = document.getElementById("checkboxCategoria").appendChild(copiaCheckbox.cloneNode(true));
+    nuevosCheckers.querySelector("label").innerHTML = event.category;
+    let nuevaClase = event.category.replace(/ /g, "");
+    nuevosCheckers.querySelector("input").setAttribute("id",nuevaClase);
+    nuevosCheckers.querySelector("label").setAttribute("for",nuevaClase);
+  }
+});
+
+let buscarTexto = document.getElementById("buacador");
+buscarTexto.addEventListener("keyup", (e) => {
+  let texto = e.target.value.toLowerCase();
+  Array.from(tarjeta).forEach((tarjeta) => {
+    let nombre = tarjeta.querySelector("h5").innerText.toLowerCase();
+    let descripcion = tarjeta.querySelector("p").innerText.toLowerCase();
+    if (nombre.includes(texto) || descripcion.includes(texto)) {
+      tarjeta.setAttribute("class", tarjeta.className.replace(" TN", " TY"));
+
+    } else {
+      tarjeta.setAttribute("class", tarjeta.className.replace(" TY", " TN"));
+    }
+  });
+  mostrarFiltrados();
+});
+
+let CheckboxCategoria = document.getElementById("checkboxCategoria")
+CheckboxCategoria.addEventListener("click", (e) => {
+  let checkbox = e.target;
+  let nombreCheckbox = checkbox.parentElement.innerText.replace(/ /g, "");
+  if (checkbox.type === "checkbox") {
+    contador++;
+  Array.from(tarjeta).forEach((tarjeta) => {
+    if (contador === 1) {
+      tarjeta.setAttribute("class", tarjeta.className.replace(" CY", " CN"));
+    }
+    if (checkbox.checked) {
+      if (tarjeta.classList.contains(nombreCheckbox)) {
+        tarjeta.setAttribute("class", tarjeta.className.replace(" CN", " CY"));
+      }
+    }else {
+      if (tarjeta.classList.contains(nombreCheckbox)) {
+        tarjeta.setAttribute("class", tarjeta.className.replace(" CY", " CN"));
+      }
+      if (contador ===  2 ) {
+        tarjeta.setAttribute("class", tarjeta.className.replace(" CN", " CY"));
+      }
+  }
+ 
+});
+if (checkbox.checked === false) {
+    contador -= 2;
+}
+mostrarFiltrados();
+  }
+});
+
+
+let numero = 0;
+
+
+function mostrarFiltrados() {
+  Array.from(tarjeta).forEach((tarjeta) => {
+    if (tarjeta.classList.contains("CY") && tarjeta.classList.contains("TY")) {
+      tarjeta.style.display = "block";
+      if (tarjeta.style.display === "block" && tarjeta.classList.contains("TY")) {
+        if (numero > 0) {
+          numero--;
+        }
+      }
+    }else {
+      tarjeta.style.display = "none";
+      if (tarjeta.style.display === "none" && tarjeta.classList.contains("TN")) {
+        if (numero <13) {
+          numero++;
+        }
+      }
+    }
+
+  });
+  if (numero === 13) {
+    noHay.style.display = "block";
+  } else {
+    noHay.style.display = "none";
+
+  }
+}
+noHay.style.display = "none";
